0ebfb25479d41271c77ddd6d9f8e48b0911cf2c4
Wilmer Martinez <info@wilmermartinez.dev>
2024-06-25 22:27:28 -0400

    Update __init__.py

--- unolet/__init__.py
+++ unolet/__init__.py
@@ -2,15 +2,36 @@ __version__ = "0.0.1"
 
 from unolet.api import UnoletAPI as Unolet
 from unolet.erp import (
+    DocumentType,
     Document,
     Invoice,
+    Purchase,
+    PurchaseOrder,
+    Quotation,
     Person,
+    Product,
+    Movement,
+    NCF,
+    AuthorizationNCF,
+    Transaction,
+    Warehouse,
 )
 
 
 __all__ = [
     "Unolet",
+    "Company",
+    "DocumentType",
     "Document",
     "Invoice",
+    "Purchase",
+    "PurchaseOrder",
+    "Quotation",
+    "Movement",
+    "NCF",
+    "AuthorizationNCF",
     "Person",
+    "Product",
+    "Transaction",
+    "Warehouse",
 ]
